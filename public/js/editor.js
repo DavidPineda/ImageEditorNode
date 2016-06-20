522a445309ffc9b3562a568537c78e2fad850516
David Pineda <david.pineda@bogcontrolbox.loc>
2016-06-20 11:42:18 -0500

    modified heigh and width canvas

--- public/js/editor.js
+++ public/js/editor.js
@@ -11,13 +11,13 @@ function loadContext(idCanvas) {
 
 function drawImageScaled(img, ctx) {
     var canvas = ctx.canvas ;
-    var hRatio = canvas.width  / img.width    ;
-    var vRatio =  canvas.height / img.height  ;
-    var ratio  = Math.min ( hRatio, vRatio );
-    var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
-    var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    ctx.drawImage(img, 0,0, img.width, img.height, centerShift_x, centerShift_y, img.width*ratio, img.height*ratio);  
+    var hRatio = canvas.width  / img.width;
+    var vRatio =  canvas.height / img.height;
+    var ratio  = Math.min(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width*ratio) / 2;
+    var centerShift_y = (canvas.height - img.height*ratio) / 2;  
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width*ratio, img.height*ratio);  
 }
 
 window.onload = function() {
