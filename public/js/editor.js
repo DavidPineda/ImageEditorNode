47a17d95b0c5e37420af229a371391cb3f3c2226
David Pineda <david.pineda@bogcontrolbox.loc>
2016-06-20 11:05:52 -0500

    new function for render image

--- public/js/editor.js
+++ public/js/editor.js
@@ -9,13 +9,24 @@ function loadContext(idCanvas) {
     return false;
 }
 
+function drawImageScaled(img, ctx) {
+    var canvas = ctx.canvas ;
+    var hRatio = canvas.width  / img.width    ;
+    var vRatio =  canvas.height / img.height  ;
+    var ratio  = Math.min ( hRatio, vRatio );
+    var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+    var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.drawImage(img, 0,0, img.width, img.height, centerShift_x, centerShift_y, img.width*ratio, img.height*ratio);  
+}
+
 window.onload = function() {
     var ctx = loadContext('myCanvas');
     if(ctx){
-        var img = new Image(200, 200);
+        var img = new Image();
         img.src = document.getElementById('myImage').value;
         img.onload = function() {
-            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+            drawImageScaled(img, ctx);
         }
     }
 }
